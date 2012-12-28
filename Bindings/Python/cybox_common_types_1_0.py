e8dd1e88b451ad9cb9ccd32a81976653c5f5c063
Ivan Kirillov <ikirillov@mitre.org>
2012-12-28 09:31:17 -0500

    Minor updates

--- Bindings/Python/cybox_common_types_1_0.py
+++ Bindings/Python/cybox_common_types_1_0.py
@@ -4042,7 +4042,7 @@ class StringObjectAttributeType(BaseObjectAttributeType):
         self.exportAttributes(outfile, level, already_processed, namespace_, name_='StringObjectAttributeType')
         if self.hasContent_():
             outfile.write('>')
-            outfile.write(str(self.valueOf_).encode(ExternalEncoding))
+            outfile.write(str(quote_xml(self.valueOf_)).encode(ExternalEncoding))
             self.exportChildren(outfile, level + 1, namespace_, name_, pretty_print=pretty_print)
             outfile.write('</%s%s>%s' % (namespace_, name_, eol_))
         else:
